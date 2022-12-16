fe2f05b41d34ada13f24d44985ecef1400782888
Samuel Greco <samuel.workenv@gmail.com>
2022-12-16 12:18:41 +0100

    First init page

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
 export default function Welcome(){
     return(
-        <>
-        Welcome
-        </>
+        <Box height='100vh' display='flex' flexDirection='column'>
+            <Box border='2px solid red'>
+                <img src='/targhetta.png' width='100%'/>
+            </Box>
+
+            <Box border='2px solid green'>
+                <Typography variant="h5" textAlign='center'>
+                    Welcome into the line
+                </Typography>
+            </Box>
+        </Box>
     );
 }
