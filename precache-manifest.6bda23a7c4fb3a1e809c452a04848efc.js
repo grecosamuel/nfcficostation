27c3736deb730683fef222dba6cd801f1a8ff577
Samuel Greco <samuel.workenv@gmail.com>
2022-12-17 15:11:07 +0100

    Updates

---
+++ precache-manifest.6bda23a7c4fb3a1e809c452a04848efc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c2fcc52e585f384106b",
+    "url": "/nfcficostation/static/css/main.037a8864.chunk.css"
+  },
+  {
+    "revision": "9c2fcc52e585f384106b",
+    "url": "/nfcficostation/static/js/main.9c2fcc52.chunk.js"
+  },
+  {
+    "revision": "f414d87cc442eb8a9175",
+    "url": "/nfcficostation/static/js/1.f414d87c.chunk.js"
+  },
+  {
+    "revision": "fca11a662699dfde75c1",
+    "url": "/nfcficostation/static/js/runtime~main.fca11a66.js"
+  },
+  {
+    "revision": "316890cde238791cff008870a67a960d",
+    "url": "/nfcficostation/static/media/Bright Young Things 2.316890cd.ttf"
+  },
+  {
+    "revision": "031c927823082e35e1fde3de3acfaf38",
+    "url": "/nfcficostation/index.html"
+  }
+];
